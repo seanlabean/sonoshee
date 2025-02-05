fea262eb82a802015a6c895ba366456d9c584183
Sean Lewis <sean.phys@gmail.com>
2025-02-04 22:18:17 -0500

    working icn to png!

--- icn2png.py
+++ icn2png.py
@@ -1,17 +1,27 @@
 from PIL import Image
 from PIL import ImageOps
-import numpy as np
-import binascii
 
-with open('../uxn/test.icn', 'rb') as f:
-    chunks = [chunk for chunk in iter(lambda: f.read(8), b'')]
-    # for chunk in iter(lambda: f.read(8), b''):
-        # b = binascii.hexlify(chunk)
-    # Gotta figure something out here. How to collect these chunks into a convertable image?
-    # can get this 8x8 by 8x8 and concatenate into rows, then concat the rows. 
-    # How to know when a row ends though? Noodles have 48 8x8 images. Let's start with that.
+with open('map.icn', 'rb') as f:
+    images = [ImageOps.invert(Image.frombytes(mode='1', size=[8,8], data=chunk)) for chunk in iter(lambda: f.read(8), b'')]
 
+# gather and concatenate images in rows of 72
+rows = []
+n=1
+for n in range(1,47):
+    x_offset=0
+    row = images[72*(n-1):72*n]
+    row_im = Image.new('1', (len(row)*8, 8))
+    for im in row:
+        row_im.paste(im, (x_offset,0))
+        x_offset += im.size[0]
+    rows.append(row_im)
 
-# Mode is '1' for 1-bit
-i = ImageOps.invert(Image.frombytes(mode='1', size=[8,8], data=chunk))
-i.save('test.png')
+y_offset = 0
+final_im = Image.new('1', (rows[0].size[0], len(rows)*8))
+for row in rows:
+    final_im.paste(row, (0,y_offset))
+    y_offset += 8
+
+final_im.save('ifthisworks.png')
+
+# noodles are 568 x 368; 26128 bytes
