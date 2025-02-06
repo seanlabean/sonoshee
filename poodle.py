e6ec3898100d5ff7589b22498e903da17b119d0c
Sean Lewis <sean.phys@gmail.com>
2025-02-06 09:30:02 -0500

    some cute logging

--- poodle.py
+++ poodle.py
@@ -1,38 +1,44 @@
 from PIL import Image
 from PIL import ImageOps
 import argparse
-import time
 
 def main(icn_name):
-    tock = time.time()
-    print(f"Let's Poodle this Noodle: {icn_name}")
-    with open(icn_name, 'rb') as f:
-        images = [ImageOps.invert(Image.frombytes(mode='1', size=[8,8], data=chunk)) for chunk in iter(lambda: f.read(8), b'')]
+    if icn_name.split('.')[-1] != 'icn':
+        print(f"Uh oh. {icn_name} is not a .icn file, I can't Poodle this.")
+        return
+    try:
+        print(f"{icn_name} :: Let's Poodle this Noodle")
+        with open(icn_name, 'rb') as f:
+            images = [ImageOps.invert(Image.frombytes(mode='1', size=[8,8], data=chunk)) for chunk in iter(lambda: f.read(8), b'')]
 
-    # gather and concatenate images in rows of 72
-    print("Building rows...")
-    rows = []
-    for n in range(1,47):
-        x_offset=0
-        row = images[72*(n-1):72*n]
-        row_im = Image.new('1', (len(row)*8, 8))
-        for im in row:
-            row_im.paste(im, (x_offset,0))
-            x_offset += im.size[0]
-        rows.append(row_im)
+        # gather and concatenate images into 46 rows of 72
+        print("~ Building rows...")
+        rows = []
+        for n in range(1,47):
+            x_shift=0
+            row = images[72*(n-1):72*n]
+            row_im = Image.new('1', (len(row)*8, 8))
+            for im in row:
+                row_im.paste(im, (x_shift,0))
+                x_shift += im.size[0]
+            rows.append(row_im)
 
-    print("Stacking rows...")
-    y_offset = 0
-    final_im = Image.new('1', (rows[0].size[0], len(rows)*8))
-    for row in rows:
-        final_im.paste(row, (0,y_offset))
-        y_offset += 8
+        print("~ Stacking rows...")
+        y_shift = 0
+        final_im = Image.new('1', (rows[0].size[0], len(rows)*8))
+        for row in rows:
+            final_im.paste(row, (0,y_shift))
+            y_shift += row.size[1]
 
-    out_name = icn_name.split('.')[0] + '.png'
-    final_im.save(out_name)
-    tick = time.time()
-    print(f"{out_name}")
-    print(f"Poodled in {tick-tock:.3f} s.")
+        out_name = icn_name.split('.')[0] + '.png'
+        final_im.save(out_name)
+        print(f"{out_name}")
+        return
+    except Exception as e:
+        print(f"Uh oh. I tried to Poodle {icn_name} but failed.")
+        print(f"Are you sure this is a Noodle to Poodle?")
+        print(e)
+        return
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
